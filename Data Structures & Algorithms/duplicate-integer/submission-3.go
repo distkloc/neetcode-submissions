d8664f42e19aa805de1f972074d27937c240b5c1
Yuichi Kanbayashi <kanbayashi@sansan.com>
2026-05-01 00:54:42 +0900

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.go
@@ -0,0 +1,12 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]bool, len(nums))
+
+    for _, num := range nums {
+        if seen[num] {
+            return true
+        }
+        seen[num] = true
+    }
+
+    return false
+}
