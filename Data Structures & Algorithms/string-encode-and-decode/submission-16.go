955381b53904d832074b32ca582bc95cc4a57c6f
Yu <kanbayashi@sansan.com>
2026-05-21 06:28:13 +0900

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.go
@@ -0,0 +1,37 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	
+	sizes := make([]string, 0, len(strs))
+
+	for _, s := range strs {
+		sizes = append(sizes, strconv.Itoa(len(s)))
+	}
+
+	return strings.Join(sizes, ",") + "#" + strings.Join(strs, "")
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	if encoded == "" {
+		return make([]string, 0, 0)
+	}
+
+	parts := strings.SplitN(encoded, "#", 2)
+	sizes := strings.Split(parts[0], ",")
+
+	var res []string
+	i := 0
+	for _, size := range sizes {
+		if size == "" {
+			continue
+		}
+
+		length, _ := strconv.Atoi(size)
+		res = append(res, parts[1][i:i+length])
+		i += length
+	}
+	return res
+}
