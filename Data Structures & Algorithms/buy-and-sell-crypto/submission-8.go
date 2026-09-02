63182663322e3713a6609d51a10e3a02224c09e8
Yu <1193849+distkloc@users.noreply.github.com>
2026-09-03 07:02:41 +0900

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+	maxP := 0
+	minPrice := prices[0]
+
+	for _, p := range prices {
+		maxP = max(maxP, p - minPrice)
+		minPrice = min(minPrice, p)
+
+		fmt.Println("p:", p, " maxP: ", maxP, " minPrice:", minPrice)
+	}
+	
+
+	return maxP
+}
